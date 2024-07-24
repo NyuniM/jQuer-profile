bf477f50fd1311696a9d27bd31001868839fd2ef
NyuniM <165041898+NyuniM@users.noreply.github.com>
2024-07-25 01:39:50 +0900

    模写コーディング　「TOP」ボタン

--- MyProfile-jQuery/js/script.js
+++ MyProfile-jQuery/js/script.js
@@ -28,4 +28,22 @@ $(function () {
       100
     );
   });
+
+  // スクロールしたときにTOPに戻るボタンを表示させる
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".pagetop").fadeIn();
+    } else {
+      $(".pagetop").fadeOut();
+    }
+  });
+  $(".pagetop").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+    return false;
+  });
 });
