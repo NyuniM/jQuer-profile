31d7a786636fdecde48fd551ad000fd1459fef0b
NyuniM <165041898+NyuniM@users.noreply.github.com>
2024-09-12 13:47:06 +0900

    スクロールをなめらかに,

--- MyProfile-jQuery/js/script.js
+++ MyProfile-jQuery/js/script.js
@@ -8,7 +8,7 @@ $(function () {
     infinite: true,
     autoplaySpeed: 2000,
     swipe: true,
-    arrows: true,
+    arrows: false,
   });
 
   // リンクのホバー時に不透明度をアニメーションで変更する
@@ -47,3 +47,58 @@ $(function () {
     return false;
   });
 });
+
+// ページ内リンクのスクロールをなめらかにする
+$(function () {
+  $('a[href^="#"]').click(function () {
+    const speed = 500;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top;
+    $("html,body").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+});
+
+
+// スクロールしたときにセクションをフェードインさせる
+$(function () {
+  $(window).scroll(function () {
+    const wHeight = $(window).height();
+    const wScroll = $(window).scrollTop();
+    $(".block").each(function () {
+      const bPosition = $(this).offset().top;
+      if (wScroll > bPosition - wHeight + 200) {
+        $(this).addClass("fadeIn");
+      }
+    });
+  });
+});
+
+// Worksの画像をクリックしたときにモーダルで拡大表示する
+$(document).ready(function() {
+  // モーダル
+  $('.modalimg').on('click', function(event) {
+    event.preventDefault();
+    const imgSrc = $(this).find('img').attr('src');
+    $('.bigimg').attr('src', imgSrc);
+    $('.modal').fadeIn();
+    $('body,html').css('overflow', 'hidden');
+  });
+
+  // 閉じるボタン
+  $('.modalclose').on('click', function() {
+    $('.modal').fadeOut();
+    $('body,html').css('overflow-y', 'visible');
+  });
+
+  // モーダル外をクリックした場合
+  $(window).on('click', function(event) {
+    if ($(event.target).hasClass('modal')) {
+      $('.modal').fadeOut();
+      $('body,html').css('overflow', 'auto');
+    }
+  });
+});
+
+
